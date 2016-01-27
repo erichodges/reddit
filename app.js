ea16c02ab30673f50e9419b76d462893be50f7bc
gregsim92 <gregsim92@gmail.com>
2016-01-27 11:02:18 -0800

    commiting before merge

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-angular.module('redditClone', []).controller('mainController', function($scope) {
-
-	$scope.postScore = 0;
+var app = angular.module('redditClone', ['ngAnimate', 'angularMoment']);
 
 
-  $scope.submitPost = function(isValid) {
-  $scope.userPosts = $scope.userPosts || [];
-    event.preventDefault();
-    if (isValid) {
-      $scope.userPosts.push($scope.userPost);
-      $scope.userPost = {};
-      $scope.postForm.$setPristine();
-      console.log($scope.userPosts);
-    }
-  };
 
-  $scope.showForm = false;
+
+  app.controller('mainController', function($scope) {
+
+    	$scope.postScore = 0;
+      window.scope = $scope;
+
+
+      $scope.submitPost = function(isValid) {
+        $scope.userPosts = $scope.userPosts || [];
+          if (isValid) {
+            $scope.userPosts.push($scope.userPost);
+            $scope.userPost = {};
+            $scope.postForm.$setPristine();
+            $scope.showForm = false;
+            console.log($scope.userPosts);
+          }
+      };
+
+      $scope.submitComment = function(isValid) {
+      $scope.comments = $scope.comments || [];
+        event.preventDefault();
+        if (isValid) {
+          $scope.comments.push($scope.userPost);
+          $scope.comment = {};
+          $scope.commentForm.$setPristine();
+          console.log($scope.comments);
+        }
+      };
+
+      $scope.showForm = false;
+      $scope.addComment = false;
 
 });
+
+
 
 
