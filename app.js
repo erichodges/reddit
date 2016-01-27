25821129e508e61914184144eccbd477f4e050bb
Eric Hodges <ehodges2004@gmail.com>
2016-01-26 16:35:05 -0800

    Added form and start of posts

---
+++ app.js
@@ -0,0 +1,20 @@
+angular.module('redditClone', []).controller('mainController', function($scope) {
+
+	$scope.postScore = 0;
+
+
+  $scope.submitPost = function(isValid) {
+  $scope.userPosts = $scope.userPosts || [];
+    event.preventDefault();
+    if (isValid) {
+      $scope.userPosts.push($scope.userPost);
+      $scope.userPost = {};
+      $scope.postForm.$setPristine();
+      console.log($scope.userPosts);
+    }
+  };
+
+
+});
+
+
