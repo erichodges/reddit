bd607468843b51c4d2e74a46506a3956b3288f8b
gregsim92 <gregsim92@gmail.com>
2016-01-27 16:48:57 -0800

    finished functionality

--- app.js
+++ app.js
@@ -5,35 +5,48 @@ var app = angular.module('redditClone', ['ngAnimate', 'angularMoment']);
 
   app.controller('mainController', function($scope) {
 
-    	$scope.postScore = 0;
-      window.scope = $scope;
+      $scope.comment = {};
+      $scope.comments = [];
 
 
-
-      $scope.submitPost = function(isValid) {
+      $scope.submitPost = function(form) {
         $scope.userPosts = $scope.userPosts || [];
-          if (isValid) {
-            $scope.userPosts.push($scope.userPost);
-            $scope.userPost = {};
-            $scope.postForm.$setPristine();
-            $scope.showForm = false;
-            console.log($scope.userPosts);
-          }
-      };
 
-      $scope.submitComment = function(isValid) {
-      $scope.comments = $scope.comments || [];
-        event.preventDefault();
-        if (isValid) {
-          $scope.comments.push($scope.userPost);
-          $scope.comment = {};
-          $scope.commentForm.$setPristine();
-          console.log($scope.comments);
+        if (form.$valid) {
+          $scope.postForm.date = moment().calendar();
+          $scope.userPost.score = 0;
+          $scope.userPosts.push($scope.userPost);
+          $scope.userPost = {};
+          $scope.postForm.$setPristine();
+          $scope.showForm = false;
+          console.log($scope.userPosts);
         }
+
       };
 
-      $scope.showForm = false;
-      $scope.addComment = false;
+      $scope.submitComment = function(form) {
+        $scope.comments = $scope.comments || [];
+        
+        if (form.$valid) {
+          $scope.addComment = false;
+          $scope.comments.push($scope.comment);
+          $scope.comment = {};
+        }
+
+      
+      };
+
+      $scope.increment = function(post){
+   
+         post.score ++;
+      }
+
+      $scope.decrement = function(post){
+         post.score --;
+      }
+
+      // $scope.showForm = false;
+      // $scope.addComment = false;
 
 });
 
